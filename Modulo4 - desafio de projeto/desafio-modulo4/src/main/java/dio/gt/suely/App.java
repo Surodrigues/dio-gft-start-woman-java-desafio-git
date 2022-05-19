66cdf20ce4a9f4ef5c293ec170c8e20647ebd3b6
Suely Rodrigues <66686508+Surodrigues@users.noreply.github.com>
2022-05-19 18:37:44 -0300

    .

--- Modulo4 - desafio de projeto/desafio-modulo4/src/main/java/dio/gt/suely/App.java
+++ Modulo4 - desafio de projeto/desafio-modulo4/src/main/java/dio/gt/suely/App.java
@@ -16,11 +16,16 @@ public final class App {
 
         Cliente t1 = new Cliente("Hosana Mendes");
         System.out.println(t1);
+        Cliente t2 = new Cliente("Thiago Silva");
+        Conta c2 = new Conta(t2, TipoConta.POUPANCA, Agencia.BEIRA_MAR);
+        c2.depositar(10000);
 
         Conta c1 = new Conta(t1, TipoConta.CORRENTE, Agencia.CENTRO);
         System.out.println(c1);
 
         c1.depositar(200.0);
+        c1.sacar(50);
+        c2.transferir(c1, 400);
         c1.imprimirExtrato();
 
 
